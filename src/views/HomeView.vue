34eec61342f07b8f30a6138dac93f2eb450494ff
PyaePhyoMaung7 <pyaephyomg1004@gmail.com>
2025-07-28 23:13:14 +0630

    add tech icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,7 +21,7 @@
         <img src="/images/cliff.png" class="cliff w-60" />
       </div>
     </section>
-    <section class="main-content absolute top-0 left-0 z-15">
+    <section class="main-content absolute top-0 left-0 z-15 overflow-hidden h-screen">
       <nav>
         <ul class="flex justify-between items-center text-lg font-bold">
           <li v-for="(tab, index) in tabs" :key="index">
@@ -63,60 +63,181 @@
           </li>
         </ul>
       </nav>
-      <section class="home grid grid-cols-2 mt-5">
-        <div class="h-120 flex items-center">
-          <div>
-            <div class="my-5 text-2xl overflow-hidden py-1">
-              <div class="intro">
-                <span
-                  v-html="formatIntroText(t('home.intro.greeting', { name: 'name' }), 'name')"
-                ></span>
+      <div class="overflow-auto h-full">
+        <div>
+          <section class="home h-130 grid grid-cols-2 mt-5">
+            <div class="flex items-center justify-center text-center">
+              <div class="-mt-15">
+                <div class="my-5 text-2xl overflow-hidden py-1">
+                  <div class="intro">
+                    <span
+                      v-html="formatIntroText(t('home.intro.greeting', { name: 'name' }), 'name')"
+                    ></span>
+                  </div>
+                </div>
+                <div class="text-2xl my-5 overflow-hidden py-1">
+                  <div class="intro">
+                    <span
+                      v-html="formatIntroText(t('home.intro.role', { role: 'role' }), 'role')"
+                    ></span>
+                  </div>
+                </div>
+                <div class="text-2xl my-5 overflow-hidden py-1">
+                  <div class="intro">
+                    <span
+                      v-html="
+                        formatIntroText(t('home.intro.closing', { action: 'action' }), 'action')
+                      "
+                    ></span>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="text-2xl my-5 overflow-hidden py-1">
-              <div class="intro">
-                <span
-                  v-html="formatIntroText(t('home.intro.role', { role: 'role' }), 'role')"
-                ></span>
+            <div class="flex justify-center items-center relative">
+              <svg-icon
+                type="mdi"
+                :path="mdiXml"
+                style="width: 50px; height: 50px"
+                class="profile-icon absolute top-15 left-40 text-[#9A5C30]"
+              ></svg-icon>
+              <svg-icon
+                type="mdi"
+                :path="mdiServerOutline"
+                style="width: 50px; height: 50px"
+                class="profile-icon absolute top-45 left-30 text-[#9A5C30]"
+              ></svg-icon>
+              <svg-icon
+                type="mdi"
+                :path="mdiDatabaseArrowUpOutline"
+                style="width: 50px; height: 50px"
+                class="profile-icon absolute top-75 left-40 text-[#9A5C30]"
+              ></svg-icon>
+              <div class="ml-20 -mt-10" style="width: 200px">
+                <img src="/images/profile.png" class="profile-img" />
               </div>
             </div>
-            <div class="text-2xl my-5 overflow-hidden py-1">
-              <div class="intro">
-                <span
-                  v-html="formatIntroText(t('home.intro.closing', { action: 'action' }), 'action')"
-                ></span>
+          </section>
+          <section class="about h-130">
+            <div style="height: 120vh">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere repellendus nostrum
+              sunt rem ea? Vero, ad et quibusdam laboriosam quae tempora reiciendis expedita.
+              Quaerat pariatur sed labore perspiciatis, possimus enim odit minima, eum consequuntur
+              hic incidunt nemo in amet est inventore vitae obcaecati, nulla asperiores quia quod
+              quo quasi. Maxime quam ratione animi libero voluptatem, voluptatum commodi recusandae
+              beatae accusamus quibusdam adipisci modi laborum voluptatibus dolorum magni? Soluta
+              sed voluptatibus, dolorem sint magnam ratione molestiae exercitationem sequi a ipsam!
+              Nihil magnam ad facere sunt nemo. Repudiandae reprehenderit et ipsa doloribus, at
+              commodi voluptatum, sit optio cupiditate nihil expedita. Recusandae fugit excepturi
+              reiciendis. Similique ad ipsa voluptatibus earum, iusto, esse ipsum voluptate magni
+              blanditiis quasi exercitationem sequi eveniet dolor quis nisi cum iste nobis, debitis
+              dolorum culpa ullam vitae. Cumque consequatur sunt recusandae quis, vitae corporis
+              repellat impedit molestiae voluptatum quam facere vel! Mollitia temporibus sint ex
+              beatae magni incidunt commodi adipisci quas eveniet similique deserunt, veniam
+              consequatur est cumque, unde itaque modi consectetur nemo at! Exercitationem
+              consectetur, tenetur iure alias eos, quidem eveniet officia voluptatum vitae, ex
+              excepturi a deserunt quasi! Eaque alias ipsam nam quidem illum officiis eos, neque
+              veritatis cumque molestias minima labore odit iure nobis a sunt laudantium dolores!
+              Laboriosam dolores exercitationem delectus sint quasi eligendi possimus aliquid nemo
+              quidem suscipit, repudiandae consequatur labore? Qui dolor unde aperiam dignissimos,
+              perspiciatis nihil aspernatur modi recusandae quae mollitia veniam officiis quas fugit
+              expedita reiciendis, nemo asperiores porro. Tenetur non, amet dicta dolorem, fugit
+              expedita, excepturi architecto mollitia beatae accusamus distinctio pariatur? Nobis
+              tenetur eum quam vitae dolores amet dolorem molestias delectus maxime architecto
+              doloribus hic ipsum suscipit dicta error expedita nemo cum illum, cupiditate possimus
+              iste natus aut! Esse temporibus laborum tempora, officia ipsa tenetur deleniti sed
+              corporis perspiciatis delectus minus, blanditiis sint dicta repudiandae, quis sit?
+            </div>
+          </section>
+          <section class="skills h-130 mt-15">
+            <div class="">
+              <div class="flex justify-center items-center gap-25 mb-15">
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/HTML5.svg"
+                  alt="html icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/CSS3.svg"
+                  alt="css icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/JavaScript.svg"
+                  alt="javascript icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/jQuery.svg"
+                  alt="jquery icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/Bootstrap.svg"
+                  alt="bootstrap icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/TailwindCSS.svg"
+                  alt="tailwind css icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/TypeScript.svg"
+                  alt="typescript icon"
+                  style="width: 40px"
+                />
+              </div>
+              <div class="flex justify-center items-center gap-25">
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/VueJs.svg"
+                  alt="vue js icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/NuxtJs.svg"
+                  alt="nuxt js icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/Veutify.svg"
+                  alt="vuetify icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/PHP.svg"
+                  alt="php icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/Laravel.svg"
+                  alt="laravel icon"
+                  style="width: 40px"
+                />
+                <img
+                  class="tech-icon cursor-pointer"
+                  src="/icons/MySQL.svg"
+                  alt="mysql icon"
+                  style="width: 40px"
+                />
               </div>
             </div>
-          </div>
+          </section>
+          <section class="experience"></section>
+          <section class="contact"></section>
         </div>
-        <div class="flex justify-center items-center relative">
-          <svg-icon
-            type="mdi"
-            :path="mdiXml"
-            style="width: 50px; height: 50px"
-            class="profile-icon absolute top-15 left-40 text-[#9A5C30]"
-          ></svg-icon>
-          <svg-icon
-            type="mdi"
-            :path="mdiServerOutline"
-            style="width: 50px; height: 50px"
-            class="profile-icon absolute top-45 left-30 text-[#9A5C30]"
-          ></svg-icon>
-          <svg-icon
-            type="mdi"
-            :path="mdiDatabaseArrowUpOutline"
-            style="width: 50px; height: 50px"
-            class="profile-icon absolute top-75 left-40 text-[#9A5C30]"
-          ></svg-icon>
-          <div class="ml-20" style="width: 200px">
-            <img src="/images/profile.png" class="profile-img" />
-          </div>
-        </div>
-      </section>
-      <section class="about"></section>
-      <section class="skills"></section>
-      <section class="experience"></section>
-      <section class="contact"></section>
+      </div>
     </section>
   </main>
 </template>
@@ -313,6 +434,25 @@ const runIntroAnimation = () => {
       },
       '-=0.8',
     )
+    .to('.profile-icon', {
+      scale: 1.1,
+      duration: 0.5,
+      ease: 'sine.inOut',
+      repeat: -1,
+      yoyo: true,
+      stagger: 0.2,
+    })
+
+  gsap.from('.tech-icon', {
+    opacity: 0,
+    scale: 0.5,
+    duration: 1,
+    ease: 'power3.out',
+    stagger: {
+      amount: 0.7,
+      from: 'start',
+    },
+  })
 }
 
 watch(locale, async () => {
